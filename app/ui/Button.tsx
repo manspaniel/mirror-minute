4ddc6cc7b024b9386b7b734dacc56c228cd64e1c
Daniel Lever <daniel@ed.com.au>
2025-08-22 09:26:03 +1000

    Fix

--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -1,18 +1,14 @@
 import type { ComponentProps } from "react";
-import { tv, type VariantProps } from "tailwind-variants";
 
-type Props = ComponentProps<"button"> & VariantProps<typeof buttonStyles>;
+type Props = ComponentProps<"button">;
 
 export function Button(props: Props) {
-  const styles = buttonStyles(props);
-
   return (
-    <button {...props} className={styles}>
+    <button
+      {...props}
+      className="inline-flex border border-black/5 backdrop-blur-md font-serif bg-white/50 hover:bg-white/60 text-lg shadow-lg rounded-full text-black px-5 py-1 items-center justify-center cursor-pointer transition-colors"
+    >
       {props.children}
     </button>
   );
 }
-
-const buttonStyles = tv({
-  base: "inline-flex border border-black/5 backdrop-blur-md font-serif bg-white/50 hover:bg-white/60 text-lg shadow-lg rounded-full text-black px-5 py-1 items-center justify-center cursor-pointer transition-colors",
-});
