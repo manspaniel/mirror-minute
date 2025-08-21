9942ac9c534adf8c237f1f74bfe05eb4e8fa98da
Daniel Lever <daniel@ed.com.au>
2025-08-21 17:50:00 +1000

    ???

--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -15,5 +15,4 @@ export function Button(props: Props) {
 
 const buttonStyles = tv({
   base: "inline-flex border border-black/5 backdrop-blur-md font-serif bg-white/50 hover:bg-white/60 text-lg shadow-lg rounded-full text-black px-5 py-1 items-center justify-center cursor-pointer transition-colors",
-  variants: {},
 });
