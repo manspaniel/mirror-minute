b2210540e28884ea92e1ccab1545335316d10dd7
Daniel Lever <daniel@ed.com.au>
2025-12-18 11:48:20 +1100

    Screens

--- app/app/Screens.tsx
+++ app/app/Screens.tsx
@@ -23,10 +23,6 @@ export function Screens() {
     screenContent = (
       <ModalScreen key="loadingModels">
         <motion.div
-          initial={{
-            opacity: 0,
-            scale: 0.95,
-          }}
           animate={{
             opacity: 1,
             scale: 1,
