8ec2a5f8e493c849b8e49e175054aae1fdd15f62
Daniel Lever <daniel@ed.com.au>
2025-12-18 11:48:05 +1100

    Tweaks

--- app/app/Screens.tsx
+++ app/app/Screens.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import { type ReactNode } from "react";
 import { CameraAccess } from "~/content/CameraAccess";
 import { LandingScreen } from "~/content/LandingScreen";
@@ -6,15 +6,51 @@ import { ModalScreen } from "~/layouts/ModalScreen";
 import { appState, useAppState } from "~/state/app-state";
 import { useCameraState } from "~/state/camera-state";
 import { useChallengeState } from "~/state/challenge-state";
+import { useFaceState } from "~/state/face-state";
+import { Spinner } from "~/ui/Spinner";
 
 export function Screens() {
   const state = useAppState();
   const camera = useCameraState();
   const challenge = useChallengeState();
+  const face = useFaceState();
 
   let screenContent: ReactNode = null;
 
-  if (state.screen === "landing") {
+  const loadedModels = face.modelsLoaded;
+
+  if (!loadedModels) {
+    screenContent = (
+      <ModalScreen key="loadingModels">
+        <motion.div
+          initial={{
+            opacity: 0,
+            scale: 0.95,
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+          }}
+          exit={{
+            opacity: 0,
+            scale: 0.95,
+          }}
+          transition={{ duration: 0.3 }}
+          className="font-serif text-lg flex flex-col justify-center items-center gap-2"
+        >
+          <span>One moment...</span>
+          <span className="text-[0.8em]">
+            <Spinner />
+          </span>
+        </motion.div>
+      </ModalScreen>
+    );
+    return (
+      <AnimatePresence mode="wait" initial={true}>
+        {screenContent}
+      </AnimatePresence>
+    );
+  } else if (state.screen === "landing") {
     screenContent = (
       <ModalScreen key="landingScreen">
         <LandingScreen />
